94e69c4f568d5745946ea219c45712d7f04d0fee
loic <lgevrey@oxya.com>
2015-04-14 09:18:43 +0200

    Bugfix

--- desktop/php/calendar.php
+++ desktop/php/calendar.php
@@ -19,21 +19,21 @@ foreach ($eqLogics as $eqLogic) {
 	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
 }
 ?>
-            </ul>
-        </div>
-    </div>
+           </ul>
+       </div>
+   </div>
 
-    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-        <legend>{{Mes agendas}}
-        </legend>
-        <div class="eqLogicThumbnailContainer">
-          <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-             <center>
-                <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-            </center>
-            <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
-        </div>
-        <?php
+   <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+    <legend>{{Mes agendas}}
+    </legend>
+    <div class="eqLogicThumbnailContainer">
+      <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+       <center>
+        <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+    </center>
+    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+</div>
+<?php
 foreach ($eqLogics as $eqLogic) {
 	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
 	echo "<center>";
@@ -43,103 +43,104 @@ foreach ($eqLogics as $eqLogic) {
 	echo '</div>';
 }
 ?>
-    </div>
+</div>
+</div>
 
-    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-        <div class="row">
-            <div class="col-sm-6">
-                <form class="form-horizontal">
-                    <fieldset>
-                        <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-                        <div class="form-group">
-                            <label class="col-sm-4 control-label">{{Nom de l'équipement}}</label>
-                            <div class="col-sm-4">
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement gCalendar}}"/>
-                            </div>
+<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+    <div class="row">
+        <div class="col-sm-6">
+            <form class="form-horizontal">
+                <fieldset>
+                    <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">{{Nom de l'équipement}}</label>
+                        <div class="col-sm-4">
+                            <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                            <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement gCalendar}}"/>
                         </div>
-                        <div class="form-group">
-                            <label class="col-sm-4 control-label" >{{Objet parent}}</label>
-                            <div class="col-sm-4">
-                                <select class="eqLogicAttr form-control" data-l1key="object_id">
-                                    <option value="">{{Aucun}}</option>
-                                    <?php
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label" >{{Objet parent}}</label>
+                        <div class="col-sm-4">
+                            <select class="eqLogicAttr form-control" data-l1key="object_id">
+                                <option value="">{{Aucun}}</option>
+                                <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-4 control-label">{{Catégorie}}</label>
-                            <div class="col-sm-8">
-                                <?php
+                           </select>
+                       </div>
+                   </div>
+                   <div class="form-group">
+                    <label class="col-sm-4 control-label">{{Catégorie}}</label>
+                    <div class="col-sm-8">
+                        <?php
 foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
 	echo '<label class="checkbox-inline">';
 	echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
 	echo '</label>';
 }
 ?>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-4 control-label">{{Activer}}</label>
-                            <div class="col-sm-1">
-                                <span class="eqLogicAttr" data-l1key="isEnable" style="display: none;">1</span>
-                                <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="enableCalendar" checked/>
-                            </div>
-                            <label class="col-sm-4 control-label">{{Visible}}</label>
-                            <div class="col-sm-1">
-                                <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-4 control-label">{{Widget, nombre de jours}}</label>
-                            <div class="col-sm-2">
-                                <input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="nbWidgetDay" />
-                            </div>
-                            <label class="col-sm-4 control-label">{{Nombre d'événement maximum}}</label>
-                            <div class="col-sm-2">
-                                <input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="nbWidgetMaxEvent" />
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-8 control-label">{{Ne pas afficher le status et les commandes d'activation/désactivation}}</label>
-                            <div class="col-sm-1">
-                                <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="noStateDisplay" />
-                            </div>
-                        </div>
-
-                    </fieldset>
-                </form>
-            </div>
-            <div class="col-sm-6">
-                <form class="form-horizontal">
-                    <fieldset>
-                        <legend>{{Liste des événements de l'agenda}}</legend>
-                        <div id="div_eventList"></div>
-                        <br/>
-                        <div class="form-group">
-                            <div class="col-sm-6">
-                                <a class="btn btn-default" id="bt_addEvent"><i class="fa fa-plus-circle"></i> {{Ajouter événement}}</a>
-                            </div>
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-        </div>
-        <legend>{{Agenda}}</legend>
-        <div id="div_calendar"></div>
-        <form class="form-horizontal">
-            <fieldset>
-                <div class="form-actions">
-                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+                   </div>
+               </div>
+               <div class="form-group">
+                <label class="col-sm-4 control-label">{{Activer}}</label>
+                <div class="col-sm-1">
+                    <span class="eqLogicAttr" data-l1key="isEnable" style="display: none;">1</span>
+                    <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="enableCalendar" checked/>
                 </div>
-            </fieldset>
-        </form>
-    </div>
+                <label class="col-sm-4 control-label">{{Visible}}</label>
+                <div class="col-sm-1">
+                    <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">{{Widget, nombre de jours}}</label>
+                <div class="col-sm-2">
+                    <input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="nbWidgetDay" />
+                </div>
+                <label class="col-sm-4 control-label">{{Nombre d'événement maximum}}</label>
+                <div class="col-sm-2">
+                    <input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="nbWidgetMaxEvent" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-8 control-label">{{Ne pas afficher le status et les commandes d'activation/désactivation}}</label>
+                <div class="col-sm-1">
+                    <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="noStateDisplay" />
+                </div>
+            </div>
+
+        </fieldset>
+    </form>
+</div>
+<div class="col-sm-6">
+    <form class="form-horizontal">
+        <fieldset>
+            <legend>{{Liste des événements de l'agenda}}</legend>
+            <div id="div_eventList"></div>
+            <br/>
+            <div class="form-group">
+                <div class="col-sm-6">
+                    <a class="btn btn-default" id="bt_addEvent"><i class="fa fa-plus-circle"></i> {{Ajouter événement}}</a>
+                </div>
+            </div>
+        </fieldset>
+    </form>
+</div>
+</div>
+<legend>{{Agenda}}</legend>
+<div id="div_calendar"></div>
+<form class="form-horizontal">
+    <fieldset>
+        <div class="form-actions">
+            <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+            <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+        </div>
+    </fieldset>
+</form>
+</div>
 </div>
 
 
