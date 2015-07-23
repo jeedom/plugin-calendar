391e139ccd16520311cc55b92275cd87b33080c7
loic <lgevrey@oxya.com>
2015-07-23 12:49:36 +0200

    Revert

--- core/class/calendar.class.php
+++ core/class/calendar.class.php
@@ -384,7 +384,8 @@ class calendar_event {
 			$values['endDate'] = $_endDate;
 			$sql .= ' AND ((startDate >=:startDate
 				AND startDate <=:endDate)
-OR (until >=:startDate AND until <=:endDate))';
+				OR until >=:startDate
+				OR until = "0000-00-00 00:00:00")';
 		}
 		$sql .= ' ORDER BY startDate';
 		return DB::Prepare($sql, $values, DB::FETCH_TYPE_ALL, PDO::FETCH_CLASS, __CLASS__);
