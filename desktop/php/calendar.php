906781fdef2e81245a55e6d37eef242b794b7ba2
Loïc <loic@jeedom.com>
2019-03-22 09:42:29 +0100

    Rename object to jeeObject

--- desktop/php/calendar.php
+++ desktop/php/calendar.php
@@ -74,7 +74,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 									<select class="eqLogicAttr form-control" data-l1key="object_id">
 										<option value="">{{Aucun}}</option>
 										<?php
-										foreach (object::all() as $object) {
+										foreach (jeeObject::all() as $object) {
 											echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 										}
 										?>
